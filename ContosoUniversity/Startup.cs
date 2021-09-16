d49ca6e1074e10a714b3c86c67204e4251a99bf1
ChanusAloba <chanus.aloba@nzfunds.co.nz>
2021-09-16 09:49:18 +0800

    Add project files.

---
+++ ContosoUniversity/Startup.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using Microsoft.Extensions.Logging;
+
+namespace ContosoUniversity
+{
+    public class Startup
+    {
+        private readonly ILoggerFactory _loggerFactory;
+        private readonly IHostingEnvironment _env;
+
+        public Startup(IConfiguration configuration, ILoggerFactory loggerFactory, IHostingEnvironment env)
+        {
+            Configuration = configuration;
+            _loggerFactory = loggerFactory;
+            _env = env;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var logger = _loggerFactory.CreateLogger<Startup>();
+
+            if (_env.IsDevelopment())
+            {
+                // Development service configuration
+
+                logger.LogInformation("Development environment");
+            }
+            else
+            {
+                // Non-development service configuration
+
+                logger.LogInformation("Environment: {EnvironmentName}", _env.EnvironmentName);
+            }
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<ContosoUniversityContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("ContosoUniversityContext")));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc();
+        }
+    }
+}
